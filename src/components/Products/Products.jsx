6eaa96eee7ac00cb1e771e84bd552e01efc18ba2
pnguyenpnn-PPEnTN <pnguyen.pnn@gmail.com>
2022-06-30 22:03:59 -0400

    nav bar done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'NASA', description: 'Shirt.', price: '$25', image: 'https://img.ltwebstatic.com/images3_pi/2021/01/26/161162724167ad2689110fee16c0808f7da81f10f1_thumbnail_900x.webp'},
@@ -9,8 +10,11 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
